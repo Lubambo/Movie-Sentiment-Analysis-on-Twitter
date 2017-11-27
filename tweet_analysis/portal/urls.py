6da6e70660949c8f73e56d86c429ee8057c4317f
Felipe UL <felipeu.lubambo@gmail.com>
2017-11-27 16:45:05 -0200

    Removed trash and renamed application directory

--- tweet_analysis/portal/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from . import views
-
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^run_analysis/$', views.run_analysis, name='run_analysis')
-]
--- tweet_analysis/web_prototype/wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from dj_static import Cling
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_prototype.settings")
-
-application = Cling(get_wsgi_application())
